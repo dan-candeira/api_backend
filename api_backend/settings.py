9e7ca1b598b4758acc6bbfeeb98cbac185830c5a
dan-candeira <danielbarr16@gmail.com>
2020-08-24 16:50:55 -0300

    updated secret key

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4@s(%)h()qa+c0v4)93bpkc4)jd5^ft38#-0rz+3g*j5#97h5&'
+SECRET_KEY = open('secret.txt').read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

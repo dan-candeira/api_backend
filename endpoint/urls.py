3c12efe86aed62cbe6a22061faf709d8a48f6227
dan-candeira <danielbarr16@gmail.com>
2020-08-29 12:02:34 -0300

    added equipment, collect and loan-history endpoints

--- endpoint/urls.py
+++ endpoint/urls.py
@@ -2,12 +2,18 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
 from endpoint.patient.views import PatientViewSet
+from endpoint.equipment.views import EquipmentViewSet
+from endpoint.collect.views import CollectViewSet
+from endpoint.loan_history.views import LoanHistoryViewSet
 
 
 app_name = 'api'
 router = DefaultRouter()
 
 router.register('patient', PatientViewSet)
+router.register('equipment', EquipmentViewSet)
+router.register('collect', CollectViewSet)
+router.register('loan-history', LoanHistoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

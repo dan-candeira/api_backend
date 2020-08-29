e85c3cfe7f8e64480cbbb267aaf80578675b28d4
dan-candeira <danielbarr16@gmail.com>
2020-08-29 12:01:35 -0300

    registered Collect app to Django admin

--- endpoint/admin.py
+++ endpoint/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from endpoint.patient.models import Patient
+from endpoint.collect.models import Collect
 
 admin.site.register(Patient)
+admin.site.register(Collect)

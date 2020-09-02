61da339a11991752a05c85dcab4a42a39bd61fcf
dan-candeira <danielbarr16@gmail.com>
2020-09-02 15:23:06 -0300

    added retrive, update, delete methods

--- endpoint/loan_history/views.py
+++ endpoint/loan_history/views.py
@@ -1,22 +1,74 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, mixins
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from django.http.response import JsonResponse
+from rest_framework import status
 
 from endpoint.loan_history.serializers import LoanHistorySerializer
 from endpoint.loan_history.models import LoanHistory
+from endpoint.equipment.models import Equipment
 
 from django_filters.rest_framework import DjangoFilterBackend
 
+from bson.objectid import ObjectId
+from bson.json_util import loads
 
-class LoanHistoryViewSet(viewsets.GenericViewSet,
-                         mixins.ListModelMixin,
-                         mixins.CreateModelMixin,
-                         mixins.UpdateModelMixin,
-                         mixins.RetrieveModelMixin,
-                         mixins.DestroyModelMixin):
+
+class LoanHistoryViewSet(viewsets.ModelViewSet):
 
     # authentication_classes = (TokenAuthentication,)
     # permission_classes = (IsAuthenticated,)
     queryset = LoanHistory.objects.all()
     serializer_class = LoanHistorySerializer
     filter_backends = [DjangoFilterBackend]
+
+    def retrieve(self, request, *args, **kwargs):
+
+        try:
+            loan_history = LoanHistory.objects.get(_id=ObjectId(kwargs['pk']))
+        except Exception:
+            raise NotFound
+
+        serializer = LoanHistorySerializer(loan_history)
+
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+
+        try:
+            loan_history = LoanHistory.objects.get(_id=ObjectId(kwargs['pk']))
+        except Exception:
+            raise NotFound
+
+        loan_history_id = loan_history._id
+        loan_history.delete()
+
+        return JsonResponse({'message': f'loan history {loan_history_id} was deleted.'}, status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+
+        data = loads(request.body)
+
+        try:
+            loan_history = LoanHistory.objects.get(_id=ObjectId(kwargs['pk']))
+        except Exception:
+            raise NotFound
+
+        for (key, value) in data.items():
+            if (key == 'devolution_date'):
+                setattr(loan_history, key, value)
+        loan_history.save()
+
+        devolution_date = loan_history.devolution_date
+
+        if (devolution_date != None):
+            equipment = Equipment.objects.get(
+                _id=loan_history.equipment_id)
+            setattr(equipment, 'available', True)
+            equipment.save()
+
+        serializer = LoanHistorySerializer(loan_history)
+
+        return Response(serializer.data)

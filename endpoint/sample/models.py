470da9c20364153dcb316a13983e80d39fad442d
dan-candeira <danielbarr16@gmail.com>
2020-09-02 15:17:54 -0300

    added foreign field collet

--- endpoint/sample/models.py
+++ endpoint/sample/models.py
@@ -4,10 +4,10 @@ from endpoint.collect.models import Collect
 
 
 class Sample(models.Model):
-    _id = models.AutoField(primary_key=True)
+    _id = models.ObjectIdField(primary_key=True)
     header = models.JSONField(null=False, editable=False)
     time_start = models.DateTimeField(editable=False)
     time_end = models.DateTimeField(editable=False)
     captured_data = models.JSONField(editable=False, null=False, blank=False)
     collect = models.ForeignKey(
-        to=Collect, on_delete=models.CASCADE, to_field="_id")
+        to=Collect, to_field='_id', on_delete=models.CASCADE)

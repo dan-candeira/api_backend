c3afbf90000a57df1a34341f74a7e95774356be8
dan-candeira <danielbarr16@gmail.com>
2020-09-24 09:44:23 -0300

    added sensor and sensor type endpoints.

--- endpoint/urls.py
+++ endpoint/urls.py
@@ -6,6 +6,8 @@ from endpoint.equipment.views import EquipmentViewSet
 from endpoint.collect.views import CollectViewSet
 from endpoint.loan_history.views import LoanHistoryViewSet
 from endpoint.sample.views import SampleViewSet
+from endpoint.sensor_type.views import SensorTypeViewset
+from endpoint.sensor.views import SensorViewSet
 
 
 app_name = 'api'
@@ -16,6 +18,8 @@ router.register('equipment', EquipmentViewSet)
 router.register('collect', CollectViewSet)
 router.register('loan-history', LoanHistoryViewSet)
 router.register('sample', SampleViewSet)
+router.register('sensor-type', SensorTypeViewset)
+router.register('sensor', SensorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

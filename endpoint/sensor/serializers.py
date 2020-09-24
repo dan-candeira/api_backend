03a987cf4abc9c1c7e7312910040f22bb97afd26
dan-candeira <danielbarr16@gmail.com>
2020-09-24 09:39:07 -0300

    added sensor type validation

--- endpoint/sensor/serializers.py
+++ endpoint/sensor/serializers.py
@@ -1,10 +1,26 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.exceptions import NotFound
+
+from endpoint.sensor.models import Sensor
+from endpoint.sensor_type.models import SensorType
+from bson.objectid import ObjectId
 
 
 class SensorSerializer(serializers.ModelSerializer):
+    types = serializers.ListField(required=True)
+
+    class Meta:
+        model = Sensor
+        fields = ("__all__")
 
     def create(self, validated_data):
+        _types = validated_data['types']
+        for index in range(len(_types)):
+            try:
+                sensor_type = SensorType.objects.get(
+                    _id=ObjectId(_types[index]))
+            except Exception:
+                raise NotFound(
+                    detail="A sensor type with this _id was not found.", code=status.HTTP_404_NOT_FOUND)
+        validated_data['types'] = _types
         return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)

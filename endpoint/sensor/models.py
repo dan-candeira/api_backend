5f8abb27aeaaeeb162b8c72433c90a601db129b2
dan-candeira <danielbarr16@gmail.com>
2020-09-24 09:37:45 -0300

    modified auto field to objectId field, modified foreign key field to Json field.

--- endpoint/sensor/models.py
+++ endpoint/sensor/models.py
@@ -4,7 +4,6 @@ from endpoint.sensor_type.models import SensorType
 
 
 class Sensor(models.Model):
-    _id = models.AutoField(primary_key=True)
-    types = models.ArrayReferenceField(
-        to=SensorType, to_field='_id', null=False)
-    model = models.CharField(max_length=255)
+    _id = models.ObjectIdField(primary_key=True)
+    types = models.JSONField(null=False)
+    _model = models.CharField(max_length=255)

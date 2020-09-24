6b6eb2e416ee8a7a4fa90b3b1f125f5ef357d4d0
dan-candeira <danielbarr16@gmail.com>
2020-09-24 09:20:25 -0300

    added patient to loan history

--- endpoint/loan_history/serializers.py
+++ endpoint/loan_history/serializers.py
@@ -5,10 +5,13 @@ from django.utils.translation import gettext_lazy as _
 
 from endpoint.loan_history.models import LoanHistory
 from endpoint.equipment.models import Equipment
+from endpoint.patient.models import Patient
+from bson.objectid import ObjectId
 
 
 class LoanHistorySerializer(serializers.ModelSerializer):
-    devolution_date = serializers.DateTimeField()
+    devolution_date = serializers.DateTimeField(required=False)
+    patient = serializers.CharField(required=True)
 
     class Meta:
         model = LoanHistory
@@ -18,6 +21,15 @@ class LoanHistorySerializer(serializers.ModelSerializer):
 
         recieved_data = validated_data
         equipment = recieved_data['equipment']
+        patient_id = recieved_data['patient']
+
+        try:
+            patient = Patient.objects.get(_id=patient_id)
+        except Exception:
+            raise NotFound(
+                detail=f"A patient with _id:{patient_id} does not exists.")
+
+        recieved_data['patient'] = patient
 
         if (equipment.available):
             equipment.available = False

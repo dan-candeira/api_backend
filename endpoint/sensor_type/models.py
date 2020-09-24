0d0f5db4d1b636858259a435aae66e03fa6d2269
dan-candeira <danielbarr16@gmail.com>
2020-09-24 09:41:12 -0300

    modified auto field to objectId field.

--- endpoint/sensor_type/models.py
+++ endpoint/sensor_type/models.py
@@ -2,7 +2,7 @@ from djongo import models
 
 
 class SensorType(models.Model):
-    _id = models.AutoField(primary_key=True)
+    _id = models.ObjectIdField(primary_key=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     mesuaring_dimentions = models.JSONField(
         blank=False, null=False, default=[])

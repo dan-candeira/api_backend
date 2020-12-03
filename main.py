f62f0b075dbf1b46f59cd5a0ed075bd7da70646b
dan-candeira <danielbarr16@gmail.com>
2020-12-03 11:37:47 -0300

    feat: add collect url path

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.user import router as UserRouter
 from routes.token import router as TokenRouter
 from routes.patient import router as PatientRouter
 from routes.equipment import router as EquipmentRouter
+from routes.collect import router as CollectRouter
 
 app = FastAPI(
     title="Motion Sense"
@@ -20,3 +21,4 @@ app.include_router(UserRouter, prefix='/users')
 app.include_router(TokenRouter, prefix='/token')
 app.include_router(PatientRouter, prefix='/patients')
 app.include_router(EquipmentRouter, prefix='/equipments')
+app.include_router(CollectRouter, prefix='/collects')

30cb2b2f5b516cb25a8e1c529e80d072f506235a
dan-candeira <danielbarr16@gmail.com>
2020-12-03 10:53:52 -0300

    feat(helpers): add validate_patient function

--- helpers/patient.py
+++ helpers/patient.py
@@ -1,6 +1,9 @@
-from models.patient import Patient
 from models.collect import Collect
 from db import db
+
+
+from fastapi import HTTPException, status
+
 
 async def get_patient_collects():
     for patient in db.patients.find():
@@ -8,4 +11,15 @@ async def get_patient_collects():
         for collect in db.collects.find({'patient': patient['_id']}):
             collects.append(Collect(**collect))
         db.patient.update({'_id': patient['_id']}, {'$set': {'collects': collects}})
-    
+
+
+async def validate_patient(patient_id: str):
+    patient = db.patients.find_one({'_id': patient_id})
+
+    if patient is None:
+        raise HTTPException(
+            status_code= status.HTTP_404_NOT_FOUND,
+            detail="a patient with this id do not exists"
+        )
+    else:
+        return patient

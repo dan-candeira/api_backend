aadd6dd6e64e96a76b27f63911321708f2002d0b
dan-candeira <danielbarr16@gmail.com>
2020-12-02 23:27:05 -0300

    feat: add patient & equipment urls path

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from starlette.responses import RedirectResponse
 
 from routes.user import router as UserRouter
 from routes.token import router as TokenRouter
+from routes.patient import router as PatientRouter
+from routes.equipment import router as EquipmentRouter
 
 app = FastAPI(
     title="Motion Sense"
@@ -16,3 +18,5 @@ def read_root():
 
 app.include_router(UserRouter, prefix='/users')
 app.include_router(TokenRouter, prefix='/token')
+app.include_router(PatientRouter, prefix='/patients')
+app.include_router(EquipmentRouter, prefix='/equipments')

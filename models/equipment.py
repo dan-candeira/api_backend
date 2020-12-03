c36ecb2f71cfb5d7741bfb6f3755234ff00d1318
dan-candeira <danielbarr16@gmail.com>
2020-12-02 23:24:17 -0300

    feat(models): add equipment data model

---
+++ models/equipment.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+from helpers.objectid import PyObjectId
+from bson import ObjectId
+
+
+class Equipment(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    mac_address: str
+    name: str
+    description: str
+    samppling_frequency: str
+    available: bool = True
+    sensors: List[PyObjectId]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+        schema_extra = {
+            "_id": "string",
+            "mac_address": "string",
+            "name": "string",
+            "description": "string",
+            "samppling_frequency": "string",
+            "available": "false"
+        }

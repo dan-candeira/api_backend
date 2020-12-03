373baf0cc67e2be8154d225802bdd54d99b4c713
dan-candeira <danielbarr16@gmail.com>
2020-12-02 23:23:07 -0300

    feat(models): add sensor data model

---
+++ models/sensor.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+from helpers.objectid import PyObjectId
+from bson import ObjectId
+
+
+class Sensor(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    model: str
+    measuring_dimention: str
+    description: str
+    equipment: PyObjectId
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
